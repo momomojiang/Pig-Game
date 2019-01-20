d85eb3796834b4b05cb865432802c3b037df190a
rui wang <rui.wang521@gmail.com>
2019-01-20 15:46:27 -0800

    added challenges

--- app.js
+++ app.js
@@ -8,9 +8,17 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+/**
+ * 
+-Added an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. 
+- Added another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. 
+ */
 
-var scores, roundScore, activePlayer, gamePlaying;
+
+var scores, roundScore, activePlayer, gamePlaying, lastDice;
 init();
+
+
 //document.querySelector('#current-' + activePlayer).innerHTML= '<em>'+dice+'</em>'
 //var x = document.querySelector('#score-0').textContent;//just read the content or the value of the elememnt with id:'#score-0'.and store in variable x.
 //console.log(x);
@@ -18,17 +26,19 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function () {
     if (gamePlaying) {
         //1, Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
 
         //2, Display the result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
         //3, Update the round score IF the rolled number was NOT a 1
-        if (dice !== 1) {
+        if (dice1 !== 1 && dice2 !== 1) {
             //add score
-            roundScore += dice;
+            roundScore += dice1 + dice2;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
             //change to next player
@@ -47,10 +57,21 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         //update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        //undefined, 0, null or "" are coerced to false
+        // Anything else is coerced to true
+        if (input) {
+            winningScore = input;
+
+        } else {
+            winningScore = 20;
+        };
         //check if player won the game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -75,15 +96,17 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 };
 function init() {
-    gamePlaying=true;
+    gamePlaying = true;
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
